a054d75d6d1d95efe6b23bb4546a261c7e61b1cc
G3iX <savenkoserhii@knu.ua>
2022-07-14 17:31:53 +0300

    step tree done(?)

--- module5solution/js/script.js
+++ module5solution/js/script.js
@@ -115,15 +115,15 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-      //   chosenCategoryShortName
-         var homeHtmlToInsertIntoMainPage = insertProperty(randomCategoryShortName)
-         $dc.loadMenuItems("'" + homeHtmlToInsertIntoMainPage + "'")
+         chosenCategoryShortName = "'" + chosenCategoryShortName + "'"
+         var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl, "randomCategoryShortName", chosenCategoryShortName)
+      //  $dc.loadMenuItems("'" + homeHtmlToInsertIntoMainPage + "'")
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-
+      insertHtml(homeHtmlToInsertIntoMainPage)
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
